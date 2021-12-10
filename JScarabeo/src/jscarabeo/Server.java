d505acd675db84655f346fd9fb3225ec7ac7c61b
EdoardoRizzi <62106054+EdoardoRizzi@users.noreply.github.com>
2021-12-10 13:41:25 +0100

    Server
    Aggiunti gli attributi, il costruttore e il metodo run del thread server

--- JScarabeo/src/jscarabeo/Server.java
+++ JScarabeo/src/jscarabeo/Server.java
@@ -5,10 +5,48 @@
  */
 package jscarabeo;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author lazzarin_andrea
  */
-public class Server extends Thread{
-    
+public class Server extends Thread {
+    DatiCondivisi d;
+    DatagramSocket server;
+    byte[] buffer;
+    byte[] dataReceived;
+    String messaggioRicevuto;
+
+    public void Server(DatiCondivisi d) throws SocketException {
+        this.d = d;
+        this.server = new DatagramSocket(12345);
+        this.buffer = new byte[1500];
+        this.messaggioRicevuto = "";
+    }
+
+    public void run() {
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+
+        while (d.isInGame()) {
+      
+            try {
+                server.receive(packet);
+            } catch (IOException ex) {
+                Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            dataReceived = packet.getData(); // copia del buffer dichiarato sopra
+
+            messaggioRicevuto = new String(dataReceived, 0, packet.getLength());
+            d.addPacchettoRicevuto(messaggioRicevuto);
+            System.out.println(messaggioRicevuto);
+        }
+
+    }
 }
