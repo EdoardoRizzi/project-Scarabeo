7aba6ac41e1e4356ff640ad09b1685b688550e92
EdoardoRizzi <edoardorizzi979@gmail.com>
2022-01-05 14:45:52 +0100

    Update GestoreGioco.java
    Aggiunto il metodo pescaggio

--- JScarabeo/src/jscarabeo/GestoreGioco.java
+++ JScarabeo/src/jscarabeo/GestoreGioco.java
@@ -9,6 +9,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -16,21 +18,22 @@ import java.util.logging.Logger;
  *
  * @author 39334
  */
-public class GestoreGioco extends Thread{
+public class GestoreGioco extends Thread {
+
     private DatiCondivisi d;
-      private File FileCSV;
+    private File FileCSV;
+    private List<Lettera> Mano; //tessere in mano durante il turno
 
     public GestoreGioco(DatiCondivisi d) {
         this.d = d;
         this.FileCSV = new File("ValoreLettera.csv");
     }
-    
-    public void run(){
-        
+
+    public void run() {
+        Pescaggio();
     }
 
-        
-    public void generaLista(File f) throws IOException{
+    public void generaLista(File f) throws IOException {
         FileReader fr;
         try {
             fr = new FileReader(f);
@@ -41,7 +44,7 @@ public class GestoreGioco extends Thread{
             while ((linea = br.readLine()) != null) { //assegno un valore alla var linea e se è nulla non entro nel ciclo
                 v = linea.split(";");
                 char let = v[0].charAt(0);
-                int val = Integer. parseInt(v[1]);
+                int val = Integer.parseInt(v[1]);
                 Lettera l = new Lettera(let, val);
                 d.addLettera(l);
             }
@@ -50,6 +53,21 @@ public class GestoreGioco extends Thread{
             Logger.getLogger(Lettera.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    
+
+    public void Pescaggio() {
+        if (d.getListLettere() != null) {
+            
+            Random r = new Random();
+            int posLettera;
+            
+            while(Mano.size()<8)
+            {
+                posLettera = r.nextInt(0, 131);
+                if (d.getListLettere().get(posLettera) != null) {
+                    Mano.add(d.getListLettere().get(posLettera));
+                    d.removeLettera();
+                }
+            }
+        }
+    }
 }
