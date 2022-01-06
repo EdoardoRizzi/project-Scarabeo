ef808bf3c1ea6adf8f245d5438cf505088d0a9cb
EdoardoRizzi <edoardorizzi979@gmail.com>
2022-01-06 18:40:50 +0100

    Update DatiCondivisi.java
    Riordinati i metodi in DatiCondvisi

--- JScarabeo/src/jscarabeo/DatiCondivisi.java
+++ JScarabeo/src/jscarabeo/DatiCondivisi.java
@@ -20,43 +20,131 @@ public class DatiCondivisi {
     private List<Lettera> ListLettere;
     private List<String> listPacchettiRicevuti;
     private List<String> listPacchettiDaInviare;
+
     private int myScore, opponentScore;
     private int myPort, opponentPort;
     private String myIP, opponentIP;
     private String myNickname, opponentNickname;
+
     private boolean inGame, Turno;
+
     private ThreadGrafico tg;
 
     public DatiCondivisi() throws UnknownHostException {
+        this.matrice = new Lettera[17][17];
         this.listPacchettiRicevuti = new ArrayList<>();
+        this.ListLettere = new ArrayList<>();
+
         this.myPort = 666;
         this.opponentPort = 667;
         this.myScore = 0;
         this.opponentScore = 0;
         this.myIP = InetAddress.getLocalHost().getHostAddress();
         this.opponentIP = "";
+
         this.inGame = false;
         this.Turno = true;
-        this.ListLettere = new ArrayList<>();
-        this.matrice = new Lettera[17][17];
 
         this.tg = new ThreadGrafico();
     }
-
-    public ThreadGrafico getTg() {
-        return tg;
+    
+        public void addLettera(Lettera l) {
+        ListLettere.add(l);
     }
-
+    
     public void addPacchettoRicevuto(String pacchetto) {
         this.listPacchettiRicevuti.add(pacchetto);
+    }
+
+    public void addPacchettoDaInviare(String pacchetto) {
+        this.listPacchettiDaInviare.add(pacchetto);
+    }
+
+    public void removeLettera(char c) {
+        int i = 0;
+        int indexTrovato = -1;
+        boolean trovato = false;
+        while (!trovato && i < ListLettere.size()) {
+            if (ListLettere.get(i).getLettera() == c) {
+                trovato = true;
+                indexTrovato = i;
+            }
+        }
+        if (indexTrovato != -1) {
+            ListLettere.remove(i);
+        }
+    }
+
+    public Lettera[][] getMatrice() {
+        return matrice;
+    }
+
+    public void setMatrice(Lettera[][] matrice) {
+        this.matrice = matrice;
+    }
+
+    public List<Lettera> getListLettere() {
+        return ListLettere;
+    }
+
+    public void setListLettere(List<Lettera> ListLettere) {
+        this.ListLettere = ListLettere;
+    }
+
+    public List<String> getListPacchettiRicevuti() {
+        return listPacchettiRicevuti;
+    }
+
+    public void setListPacchettiRicevuti(List<String> listPacchettiRicevuti) {
+        this.listPacchettiRicevuti = listPacchettiRicevuti;
     }
 
     public List<String> getListPacchettiDaInviare() {
         return listPacchettiDaInviare;
     }
 
-    public void addPacchettoDaInviare(String pacchetto) {
-        this.listPacchettiDaInviare.add(pacchetto);
+    public void setListPacchettiDaInviare(List<String> listPacchettiDaInviare) {
+        this.listPacchettiDaInviare = listPacchettiDaInviare;
+    }
+
+    public int getMyScore() {
+        return myScore;
+    }
+
+    public void setMyScore(int myScore) {
+        this.myScore = myScore;
+    }
+
+    public int getOpponentScore() {
+        return opponentScore;
+    }
+
+    public void setOpponentScore(int opponentScore) {
+        this.opponentScore = opponentScore;
+    }
+
+    public int getMyPort() {
+        return myPort;
+    }
+
+    public void setMyPort(int myPort) {
+        this.myPort = myPort;
+    }
+
+    public int getOpponentPort() {
+        return opponentPort;
+    }
+
+    public void setOpponentPort(int opponentPort) {
+        this.opponentPort = opponentPort;
+    }
+
+    public String getMyIP() {
+        return myIP;
+    }
+
+    public void setMyIP(String myIP) {
+        this.myIP = myIP;
     }
 
     public String getOpponentIP() {
@@ -65,30 +153,6 @@ public class DatiCondivisi {
 
     public void setOpponentIP(String opponentIP) {
         this.opponentIP = opponentIP;
-    }
-
-    public boolean isInGame() {
-        return inGame;
-    }
-
-    public void setInGame(boolean inGame) {
-        this.inGame = inGame;
-    }
-
-    public int getOpponentPort() {
-        return opponentPort;
-    }
-
-    public boolean isTurno() {
-        return Turno;
-    }
-
-    public void setTurno(boolean Turno) {
-        this.Turno = Turno;
-    }
-
-    public void setOpponentPort(int opponentPort) {
-        this.opponentPort = opponentPort;
     }
 
     public String getMyNickname() {
@@ -107,38 +171,27 @@ public class DatiCondivisi {
         this.opponentNickname = opponentNickname;
     }
 
-    public int getMyPort() {
-        return myPort;
+    public boolean isInGame() {
+        return inGame;
     }
 
-    public String getMyIP() {
-        return myIP;
+    public void setInGame(boolean inGame) {
+        this.inGame = inGame;
     }
 
-    public List<String> getListPacchettiRicevuti() {
-        return listPacchettiRicevuti;
+    public boolean isTurno() {
+        return Turno;
     }
 
-    public List<Lettera> getListLettere() {
-        return ListLettere;
+    public void setTurno(boolean Turno) {
+        this.Turno = Turno;
     }
 
-    public void addLettera(Lettera l) {
-        ListLettere.add(l);
+    public ThreadGrafico getTg() {
+        return tg;
     }
 
-    public void removeLettera(char c) {
-        int i = 0;
-        int indexTrovato = -1;
-        boolean trovato = false;
-        while (!trovato && i < ListLettere.size()) {
-            if (ListLettere.get(i).getLettera() == c) {
-                trovato = true;
-                indexTrovato = i;
-            }
-        }
-        if (indexTrovato != -1) {
-            ListLettere.remove(i);
-        }
+    public void setTg(ThreadGrafico tg) {
+        this.tg = tg;
     }
 }
